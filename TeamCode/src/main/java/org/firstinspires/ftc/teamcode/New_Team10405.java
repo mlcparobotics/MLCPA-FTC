dd4430971d6c1b5d4b3f9cada83d45d08a27261f
LOCKESST <mlcparobotics@gmail.com>
2017-01-17 15:28:19 -0600

    Created New_Team10405
    Need Linear Mode in order to use sleep function.
    
    Moved batMotor and beaconMotor to gamepad2.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New_Team10405.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New_Team10405.java
@@ -34,56 +34,48 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-
-// Edited on 12/6/16 by Andres Salas
-//This program has a touch sensor. When the touch sensor is pressed, the motors are reversed.
 
 /**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
  *
  * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all iterative OpModes contain.
+ * It includes all the skeletal structure that all linear OpModes contain.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Team10405TeleOp", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
-public class Team10405TeleOp extends OpMode
-{
+@TeleOp(name="Template: Linear OpMode", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+
+public class New_Team10405 extends LinearOpMode {
+
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
-
     private DcMotor motorLeft = null;
     private DcMotor motorRight = null;
     private DcMotor beaconMotor = null;
     private DcMotor batMotor = null;
-    private TouchSensor TouchSensor = null;
+    private com.qualcomm.robotcore.hardware.TouchSensor TouchSensor = null;
     private boolean isShootersOn = false;
 
     boolean flip = false;
     float speed = 1;
 
 
+    // DcMotor leftMotor = null;
+    // DcMotor rightMotor = null;
 
-    /*
-         * Code to run ONCE when the driver hits INIT
-         */
     @Override
-    public void init() {
+    public void runOpMode() {
         telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
         /* eg: Initialize the hardware variables. Note that the strings used here as parameters
          * to 'get' must correspond to the names assigned during the robot configuration
@@ -94,78 +86,62 @@ public class Team10405TeleOp extends OpMode
         beaconMotor = hardwareMap.dcMotor.get("beaconMotor");
         batMotor = hardwareMap.dcMotor.get("batMotor");
         TouchSensor= hardwareMap.touchSensor.get("TouchSensor");
+        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
+        // rightMotor = hardwareMap.dcMotor.get("right_drive");
 
         // eg: Set the drive motor directions:
-        // Reverse the motor that runs backwards when connected directly to the battery
-        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         motorLeft.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         batMotor.setDirection(DcMotor.Direction.REVERSE);
         beaconMotor.setDirection(DcMotor.Direction.REVERSE);
-        // telemetry.addData("Status", "Initialized");
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
-
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-
-
-
-
-
-
-
-    public void start() {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
         runtime.reset();
-    }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-        telemetry.addData("Touch Sensor Value: ", TouchSensor.getValue());
-        // telemetry.addData("Touch Sensor Value: ", TouchSensor);
-        if(gamepad1.dpad_up){
-            flip = true;
-        }
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
 
-        if (gamepad1.dpad_down){
-            flip = false;
-        }
+            if(gamepad1.dpad_up){
+                flip = true;
+            }
 
-        if(flip){
-            motorRight.setPower(-gamepad1.left_stick_y/speed);
-            motorLeft.setPower(-gamepad1.right_stick_y/speed);
-        }
+            if (gamepad1.dpad_down){
+                flip = false;
+            }
 
-        else{
-            motorLeft.setPower(gamepad1.left_stick_y/speed);
-            motorRight.setPower(gamepad1.right_stick_y/speed);
-        }
+            if(flip){
+                motorRight.setPower(-gamepad1.left_stick_y/speed);
+                motorLeft.setPower(-gamepad1.right_stick_y/speed);
+            }
 
-        if(gamepad1.x){
-            speed = 4;
-        }
+            else{
+                motorLeft.setPower(gamepad1.left_stick_y/speed);
+                motorRight.setPower(gamepad1.right_stick_y/speed);
+            }
 
-        if (gamepad1.y){
-            speed = 1;
-        }
+            if(gamepad1.x){
+                speed = 4;
+            }
 
-        if (gamepad1.a){
-            motorLeft.setPower(1);
-            motorRight.setPower(1);
+            if (gamepad1.y){
+                speed = 1;
+            }
 
-        }
+            if (gamepad1.a){
+                motorLeft.setPower(1);
+                motorRight.setPower(1);
+                sleep(250);
+
+                motorLeft.setPower(-1);
+                motorRight.setPower(-1);
+                sleep(250);
+
+            }
 
 
 
@@ -200,80 +176,33 @@ public class Team10405TeleOp extends OpMode
         */
 
 
-        //Attempt to move batMotor
-        if(gamepad2.y) {
-            batMotor.setPower(1);
+            //Attempt to move batMotor
+            if(gamepad2.y) {
+                batMotor.setPower(1);
+            }
+            else if(gamepad2.x){
+                batMotor.setPower(-1);
+            }
+            else {
+                batMotor.setPower(0);
+
+
+                // set beaconMotor forward
+            }
+            if(gamepad2.a){
+                beaconMotor.setPower(.6);
+            }
+            else if(gamepad2.b) {
+                beaconMotor.setPower(-.6);
+            }
+            else {
+                beaconMotor.setPower(0);
+            }
+
+
+            // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+            // leftMotor.setPower(-gamepad1.left_stick_y);
+            // rightMotor.setPower(-gamepad1.right_stick_y);
         }
-        else if(gamepad2.x){
-            batMotor.setPower(-1);
-        }
-        else {
-            batMotor.setPower(0);
-
-
-         // set beaconMotor forward
-        }
-        if(gamepad2.a){
-            beaconMotor.setPower(.6);
-        }
-        else if(gamepad2.b) {
-            beaconMotor.setPower(-.6);
-        }
-        else {
-            beaconMotor.setPower(0);
-        }
-
-
-
-
-
-
-
-
-
-        //Get User Input
-
-       /* if(gamepad1.a){
-         leftShooter.setPower(1);
-        }
-        else
-        {
-            isShootersOn = false;
-        }
-
-        if(gamepad1.b == true && gamepad1.a == false){
-            isShootersOn = false;
-            //rightShooter.setPower(0);
-            //leftShooter.setPower(0);
-        }
-        else
-        {
-            isShootersOn = false;
-        }
-
-        if(isShootersOn)
-        {
-            rightShooter.setPower(1);
-            leftShooter.setPower(1);
-        }
-
-        if(isShootersOn)
-        {
-            rightShooter.setPower(0);
-            leftShooter.setPower(0);
-        }
-
-*/
     }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
-
-
-
-
 }
